28faded52c03ebcb06514d3b3c4e8d54bdfa5ad3
dev-xero <elo48858@gmail.com>
2024-04-18 23:58:30 +0100

    docs: Add comments to main functions

--- json.py
+++ json.py
@@ -2,23 +2,32 @@ from lexer import Lexer
 from pathlib import Path
 
 def read_file(path: str):
+    '''
+        Reads the contents of the file specified by the path.
+        - path: string file path
+    '''
     output = ''
+
     try:
         with open(path) as file:
             output = file.read()
     except:
         print("Failed to read json file.")
         exit(1)
+
     return output
 
 
 def main():
+    '''Main program entry point.'''
     path = input("> JSON File: ")
+
     if path == '':
         print("No file specified.")
         exit(1)
 
     file = read_file(path)
+    
     if file == '':
         print("Failed to read json file.")
         exit(1)
--- lexer.py
+++ lexer.py
@@ -1,4 +1,7 @@
 class Lexer:
+    '''
+    This class is responsible for reading the source file and producing valid json tokens
+    '''
     def __init__(self, source: str):
         self.source = source
 
